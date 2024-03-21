e5f1a691ade84dc7dc205a6e56c729d5c08a693c
Shiting_Huang <2654133H@student.gla.ac.uk>
2024-03-21 15:38:51 +0000

    The valve model which used integrate.ivp

--- valve.py
+++ valve.py
@@ -15,36 +15,41 @@ class Valve:
         self.diff_q=diff_q
         self.xii=0.9999 #xi, when xi=1 means valve open, xi=0 means close
         self.delta_pi=0 #the pressure difference 
-
-    def xi_t(self,delta_t):#rate of opening and closure        
+    def dxi(self,time,xi):
         if self.delta_pi>=self.poc:
-            self.xii=self.xii+(1-self.xii)*self.Kvo*(self.delta_pi-self.poc)*delta_t
-            return self.xii
-        elif self.delta_pi<self.poc:
-            self.xii=self.xii+self.xii*self.Kvc*(self.delta_pi-self.poc)*delta_t
-            return self.xii
+            return (1-xi)*self.Kvo*(self.delta_pi-self.poc)
+        if self.delta_pi<self.poc:
+            return xi*self.Kvc*(self.delta_pi-self.poc)
+
+    def xi_t(self,time):#rate of opening and closure 
+        delta_t=0.001
+        t=[time,time+delta_t]
+        dx_update=sp.integrate.solve_ivp(self.dxi,t_span=t,y0=[self.xii])
+        dx_sol=dx_update.y.flatten()[-1]
+        self.xii=dx_sol
+        return self.xii
     
-    def Aeff(self,delta_t):
+    def Aeff(self,time):
         Mst=1
         Mrg=0
         Amax=Mst*self.Aann
         Amin=Mrg*self.Aann
-        xi=self.xi_t(delta_t)
+        xi=self.xi_t(time)
         return (Amax-Amin)*xi+Amin    
     
 
-    def bernoulli_resistance(self,delta_t):#B g*cm^(-1)*s^(-2)
-        Aeff=self.Aeff(delta_t)
+    def bernoulli_resistance(self,time):#B g*cm^(-1)*s^(-2)
+        Aeff=self.Aeff(time)
         return self.rho/(2*Aeff**2)
 
-    def blood_inertance(self,delta_t):#L g*cm^(-4)
-        Aeff=self.Aeff(delta_t)
+    def blood_inertance(self,time):#L g*cm^(-4)
+        Aeff=self.Aeff(time)
         return (self.rho*self.leff)/Aeff
        
-    def delta_p(self,q,diff_q,delta_t):
+    def delta_p(self,q,diff_q,time):
         self.q=q
         self.diff_q=diff_q
-        B=self.bernoulli_resistance(delta_t)
-        L=self.blood_inertance(delta_t)
+        B=self.bernoulli_resistance(time)
+        L=self.blood_inertance(time)
         self.delta_pi=B*self.q*abs(self.q)+L*self.diff_q
         return self.delta_pi
